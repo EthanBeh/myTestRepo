9f72f015db9335f91e1388c1be2b0ab7ac24432f
BT_1N3_23 <jackeyh2@Nycstudents.net>
2023-12-15 09:04:52 -0500

    3

--- src/Ermm.java
+++ src/Ermm.java
@@ -4,4 +4,8 @@ public class Ermm {
     public static void umm() {
         System.out.println("ummm...??");
     }
+
+    public static void refusal() {
+        System.out.println("u kinda weird ngl");
+    }
 }
