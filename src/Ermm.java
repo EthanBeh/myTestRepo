d1c9aecf43f4ac1ec4deac6124fc417ab4429445
BT_1N3_24 <sample@bths.edu>
2023-12-15 09:01:36 -0500

    Ermm.umm added

---
+++ src/Ermm.java
@@ -0,0 +1,7 @@
+public class Ermm {
+    public Ermm() { }
+
+    public static void umm() {
+        System.out.println("ummm...??");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("haha dum dum");
         System.out.println("no");
+        System.out.println("tjat's not very nice of yuo >:(");
     }
 }
