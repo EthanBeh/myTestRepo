b3fcd945a8aeb1548e868e35e49daa2dff0e196c
BT_1N3_24 <sample@bths.edu>
2023-12-15 09:14:04 -0500

    spelling :)

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("haha dum dum");
         System.out.println("no");
-        System.out.println("that's not very nice of you >:(");
+        System.out.println("that's not very nice of you >:((");
         System.out.println("that's why i s aid it :)))))");
     }
 }
